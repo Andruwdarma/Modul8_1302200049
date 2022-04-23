81ff03fa523dd519847a1a7e9a7c0a92b830e325
andruwdarma <andruwdarma772@gmail.com>
2022-04-23 16:15:46 +0700

    menambahkan API Movies

---
+++ Controllers/WeattherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace modul8_1302200049.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeattherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeattherForecastController> _logger;
+
+        public WeattherForecastController(ILogger<WeattherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeattherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeattherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
